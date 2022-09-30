879731f28da50ab97876135f898ee93b1bc6eb5c
ogyammers <jamiegormannavan@gmail.com>
2022-09-30 23:02:36 +0100

    labsheet03

--- labsheet03/lab3-find-longest-word.c
+++ labsheet03/lab3-find-longest-word.c
@@ -1,8 +1,8 @@
 /*
-Program: lab3-is-sorted.c
+Program: lab3-find-longest-word.c
 Author: Jamie Gorman
-Input: array of integers
-Output: sorted array of integers
+Input: string from command line
+Output: longest word in the input string
 */
 
 // includes
@@ -13,31 +13,56 @@ Output: sorted array of integers
 // function prototypes
 void longest_word(char *token, char *longest, char str[]);
 
+
+/*
+Function: main
+Parameters: argc, argv
+Desciption: calls functions to find the longest word and
+    prints the result
+*/
 int main(int argc, char *argv[])
 {
+    // array to store the string
     char str[strlen(argv[1])];
+    
     char *token;
     char longest[20];
 
+    // copy from argv to str
     strcpy(str, argv[1]);
 
+    // call function longest_word and pass it token, longest and str
     longest_word(token, longest, str);
-
+    
+    // print the longest word
     printf("%s\n", longest);
 
     return 0;
 }
 
+
+/*
+Function: longest_word
+Parameters: token, longest, str
+Desciption: splits the string into tokens and 
+    compares it to the current longest word 
+*/
 void longest_word(char *token, char *longest, char str[])
 {
+    // first token from str on the delimiter of " " a single space
     token = strtok(str, " ");
+    // copy first token to longest
     strcpy(longest, token);
+    // while token is not NULL ie, we are still taking slices from the string
     while (token != NULL)
     {
+        // compare current token to current longest
         if (strlen(token) > strlen(longest) == 1)
         {
+            // if longer copy token to longest
             strcpy(longest, token);
         }
+        // take the next token
         token = strtok(NULL, " ");
     }
 }
--- labsheet03/lab3-leap-year.c
+++ labsheet03/lab3-leap-year.c
@@ -1,8 +1,8 @@
 /*
-Program: lab3-is-sorted.c
+Program: lab3-leap-year.c
 Author: Jamie Gorman
-Input: array of integers
-Output: sorted array of integers
+Input: starting year and ending year
+Output: all the leap years from starting year to ending year
 */
 
 // includes
@@ -10,25 +10,38 @@ Output: sorted array of integers
 #include <stdlib.h>
 #include <string.h>
 
-//function prototypes
+// function prototypes
 void leap_years(int start, int end);
 
-
+/*
+Function: main
+Parameters: argc, argv
+Desciption: calls function to find leap years
+*/
 int main(int argc, char *argv[])
 {
     int start = atoi(argv[1]);
     int end = atoi(argv[2]);
 
     leap_years(start, end);
-    //printf("%d - %d\n", start, end);
+    // printf("%d - %d\n", start, end);
     return 0;
 }
 
+/*
+Function: leap_years
+Parameters: start year and end year
+Desciption: algorithm to print all leap years
+*/
 void leap_years(int start, int end)
 {
+    // finds the first leap year that is <= the start year
     int year = ((start + 3) / 4) * 4;
+    // loop until year is <= the end year
     while (year <= end)
     {
+        // check the edge cases ie centuries
+        // if divisible by 100 a leap year must also be divisible by 400
         if (year % 100 == 0)
         {
             if (year % 400 == 0)
@@ -36,13 +49,12 @@ void leap_years(int start, int end)
                 printf("%d\n", year);
             }
         }
+        // all other cases
         else
         {
             printf("%d\n", year);
         }
+        // increment by 4, ie next leap year
         year += 4;
-        
     }
-    
-
 }
